b4b0e410351eea08f3bd7948596a4e3b02163e00
Podchasov Oleg <podchasov.oleg@gmail.com>
2021-03-17 23:35:01 +0300

    finish basic functionality

---
+++ app/src/main/java/com/example/calculator_app/ActiveOperand.java
@@ -0,0 +1,29 @@
+package com.example.calculator_app;
+
+public class ActiveOperand {
+    Operand operand1;
+    Operand operand2;
+    Operand active;
+
+    public ActiveOperand(Operand operand1, Operand operand2) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+        this.active = operand1;
+    }
+
+    public void toggleActive() {
+        this.active = this.active == this.operand1 ? this.operand2 : this.operand1;
+    }
+
+    public Operand getActive() {
+        return this.active;
+    }
+
+    public Operand getInactive() {
+        return this.active == this.operand1 ? this.operand2 : this.operand1;
+    }
+
+    public void clear() {
+        this.active = operand1;
+    }
+}
